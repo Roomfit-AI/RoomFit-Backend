b8d4c1a1eb2304dbb299ca13d591b450e69a165b
SJA <jashin0414@khu.ac.kr>
2026-07-10 13:58:21 +0900

    Add frontend dev CORS origins

--- src/main/java/com/roomfit/config/WebConfig.java
+++ src/main/java/com/roomfit/config/WebConfig.java
@@ -10,8 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String[] ALLOWED_ORIGINS = {
             "http://localhost:3000",
             "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
             "http://127.0.0.1:3000",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "http://127.0.0.1:5174",
+            "http://127.0.0.1:5175"
     };
 
     @Override
