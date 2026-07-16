a2ef01c10d80565fd83e0823ddfdd1bdcdc66cef
SJA <jashin0414@khu.ac.kr>
2026-07-16 20:36:38 +0900

    test: isolate integration test database

--- src/test/java/com/roomfit/placement/LayoutFeedbackControllerTest.java
+++ src/test/java/com/roomfit/placement/LayoutFeedbackControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.everyItem;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class LayoutFeedbackControllerTest {
 
     @Autowired
--- src/test/java/com/roomfit/placement/LayoutRecommendControllerTest.java
+++ src/test/java/com/roomfit/placement/LayoutRecommendControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasItems;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Transactional
 class LayoutRecommendControllerTest {
 
     @Autowired
